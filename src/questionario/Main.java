6218fcc285af3b0597a8fe9a3cd5966c2356c730
Fabricio Santos Monteiro <fabriciosantos47@gmail.com>
2018-11-30 12:11:55 -0200

    feat: cria telas Home e Novo Form

--- src/questionario/Main.java
+++ src/questionario/Main.java
@@ -1,8 +1,11 @@
 package questionario;
 
+import view.Home;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Home home = new Home();
+        home.setVisible(true);
     }
     
 }
