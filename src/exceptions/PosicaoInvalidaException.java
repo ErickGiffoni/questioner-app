58d6d4190715bff41404824a041468693dde2d52
ErickGiffoni <giffoni.erick@gmail.com>
2018-12-02 16:47:28 -0200

    feat - nova excessao

---
+++ src/exceptions/PosicaoInvalidaException.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+/**
+ *
+ * @author erick.giffoni
+ */
+public class PosicaoInvalidaException extends Exception{
+
+    @Override
+    public void Exception(String Nome) {
+        setNome("Posição Inválida Infomrada");
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String toString() {
+        return getNome();//????
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
