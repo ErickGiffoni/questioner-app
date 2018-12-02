8737f63c5bb2c4d922785a9362973ad404f0486d
ErickGiffoni <giffoni.erick@gmail.com>
2018-12-02 16:12:24 -0200

    feat - classe Fechada implementada

---
+++ src/formularios/Fechada.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formularios;
+
+/**
+ *
+ * @author erick.giffoni
+ */
+public class Fechada extends Questao{
+    private int numeroDeAlternativas;
+    public int getNumeroDeAlternativas(){
+        return this.numeroDeAlternativas;
+    }
+    public void setNumeroDeAlternativas(int numero){
+        this.numeroDeAlternativas = numero;
+    }
+}
