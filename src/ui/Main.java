a0946ee94b7010a987c196db405e43811a778a26
ErickGiffoni <giffoni.erick@gmail.com>
2018-12-03 10:32:20 -0200

    feat - main tem varios formularios

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,9 +1,11 @@
 package ui;
 
+import formularios.Formulario;
 import view.Home;
 
 public class Main {
     public static void main(String[] args) {
+        Formulario[] formularios;//a main tem varios formularios 
         Home home = new Home();
         home.setVisible(true);
     }
