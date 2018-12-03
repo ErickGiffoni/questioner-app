6b7c7ce44f3fd6debc17a7a861ad1be18c086f15
Fabricio Santos Monteiro <fabriciosantos47@gmail.com>
2018-12-03 10:11:03 -0200

    style: remove comentários do código gerados pela IDE

--- src/excecao/Excecoes.java
+++ src/excecao/Excecoes.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package excecao;
 
-/**
- *
- * @author lucas
- */
-public class DescricaoObrigatoriaNaoInformadaException extends Exception {
+public class Excecoes extends Exception {
     
 }
--- src/exceptions/Exception.java
+++ src/exceptions/Exception.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exceptions;
 
-/**
- *
- * @author erick.giffoni
- */
 public abstract class Exception extends java.lang.Exception{
     private String nome;
     public abstract void Exception();//construtor da classe abstrata
@@ -21,4 +12,4 @@ public abstract class Exception extends java.lang.Exception{
         this.nome = Nome;
     }
     
-}//end of class Exception
+}
