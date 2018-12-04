4eccab44fa0ffd7ce2691cad06061ee0c3bc065b
LucasLopes <l.lopes.fga@mail.com>
2018-12-04 14:19:08 -0200

    Criação de classe de efeitos

---
+++ src/gui/TextField.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class TextField implements ActionListener {
+ JTextField tf;
+ JLabel lbl;
+ JFrame f;  
+ JButton btn = new JButton("OK");
+    public TextField(String Formulario,String Pergunta){
+        f= new JFrame(Formulario);
+        
+        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        f.setLayout(new FlowLayout());
+        lbl = new JLabel(Pergunta);
+        tf = new JTextField(30);
+        
+        tf.setText("");
+        f.add(lbl);
+        f.add(tf);
+        f.add(btn);
+        btn.addActionListener(this);
+        f.setVisible(true);
+        f.setSize(500,100);
+        
+        
+        
+    }
+    
+    public void actionPerformed(ActionEvent ae){
+        JOptionPane.showMessageDialog(null, tf.getText());
+        f.dispose();
+    }
+    
+//    public static void main(String[] args) {
+//        new TextField("Questao","Seu nome");
+//    }
+}
