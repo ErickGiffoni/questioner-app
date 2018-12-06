61689fb338f549fd15ef5c75e216ae6cfa16b0f5
LucasLopes <l.lopes.fga@mail.com>
2018-12-06 11:36:01 -0200

    classe Aberta com gui

---
+++ src/gui/JOAberta.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import formularios.Questao;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author lucas
+ */
+public class JOAberta extends Questao {
+    private String resposta;
+
+    public String getResposta() {
+        return resposta;
+    }
+
+    public void setResposta(String resposta) {
+        this.resposta = resposta;
+    }
+    public JOAberta(String nome, String enunciado) {
+        super(nome, enunciado);
+    }
+    public void chamaGui(){
+        String tmp;
+        String e = getEnunciado(), t= getNome();
+        tmp = JOptionPane.showInputDialog(null, e, t, JOptionPane.PLAIN_MESSAGE);
+        setResposta(tmp);
+    }
+    
+    public static void main(String[] args) {
+        JOAberta joa = new JOAberta("titulo","seu nome");
+        joa.chamaGui();
+        System.out.println(joa.getResposta());
+    }
+            
+}
