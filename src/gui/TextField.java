61b8bb5695a2dd26e5e8a8879174c5d857a0bc57
LucasLopes <l.lopes.fga@mail.com>
2018-12-07 05:24:57 -0200

    criação do  método initGui()

--- src/gui/TextField.java
+++ src/gui/TextField.java
@@ -9,11 +9,29 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 public class TextField implements ActionListener {
+    String nome,enunciado;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEnunciado() {
+        return enunciado;
+    }
+
+    public void setEnunciado(String enunciado) {
+        this.enunciado = enunciado;
+    }
  JTextField tf;
  JLabel lbl;
  JFrame f;  
  JButton btn = new JButton("OK");
 
+ 
     public String getRetorno() {
         return retorno;
     }
@@ -23,12 +41,14 @@ public class TextField implements ActionListener {
     }
  
  String retorno;
-    public TextField(String Formulario,String Pergunta){
-        f= new JFrame(Formulario);
+ public void initGui()
+ {
+     
+        f= new JFrame(nome);
         
         f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             f.setLayout(new FlowLayout());
-        lbl = new JLabel(Pergunta);
+        lbl = new JLabel(enunciado);
         tf = new JTextField(30);
         
         tf.setText("");
@@ -37,19 +57,33 @@ public class TextField implements ActionListener {
         f.add(btn);
         btn.addActionListener(this);
         f.setVisible(true);
+        f.setLocation(200, 200);
         f.setSize(500,100);
         
-        
+
+}
+ 
+    public TextField(String Formulario,String Pergunta){
+        setNome(Formulario);
+        setEnunciado(Pergunta);       
         
     }
   
     public void actionPerformed(ActionEvent ae){
+        if(tf.getText().isEmpty())
+        {
+           // f.dispose();
+            
+            
+        }else
         JOptionPane.showMessageDialog(null, tf.getText());
         this.setRetorno(tf.getText());
         f.dispose();
     }
     
 //    public static void main(String[] args) {
-//        new TextField("Questao","Seu nome");
+//       TextField tf =new TextField("Questao","Seu nome");
+//        
+//        tf.initGui();
 //    }
 }
